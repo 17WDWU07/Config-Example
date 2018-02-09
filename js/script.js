850815213fe3517999af109ba846d69b57ef5b41
Richard Hpa <richard.hpa@acgedu.com>
2018-02-09 14:54:49 +1300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var ClientID = '35fa472dd056452b83a5e587bfdc7193';
+var RedirectURI = 'http://192.168.33.10/Instagram/';
+var accessToken = '261990581.35fa472.758e4f42657c46e2b3b2fc4fc715b1d7'
+
+$.ajax({
+    url: ,
+    dataType: 'json',
+    success: function(instagramData){
+
+    },
+    error: function(error){
+        console.log(error);
+        console.log("Something has gone wrong");
+    }
+})
